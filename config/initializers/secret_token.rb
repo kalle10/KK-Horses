0195cfd44e290c93673cd4e7ca11d9e8e8e0248b
kalle <malmiharju@btinternet.com>
2014-02-27 17:27:23 +0000

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KkHorses::Application.config.secret_key_base = 'fe9373fc2304f93abac26ea990686531c17f06aec8ccc824b1e7f394a1928a6e60d566d1f3b66d80340dfd39cab8b713a5d9416476b413ef1341deede3a210b1'
